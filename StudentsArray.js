aad1717fd5acbd404617cf6f64fedceb76a8da15
Mohamed Adel <badrmohammed602@gmail.com>
2023-04-17 15:06:44 +0200

    initial commit

---
+++ StudentsArray.js
@@ -0,0 +1,23 @@
+export const students = [
+    {"id": 1,
+    "name": "Mohamed"
+    },
+    {"id": 2,
+    "name": "Amr"
+    },
+    {"id": 3,
+    "name": "Taylor"
+    },
+    {"id": 4,
+    "name": "Jennifer"
+    },
+    {"id": 5,
+    "name": "De Niro"
+    },
+    {"id": 6,
+    "name": "Menna"
+    },
+    {"id": 7,
+    "name": "Rana"
+    }
+]
